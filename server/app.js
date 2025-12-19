89c0e5d993ca69dbb2bbbea33a0e393ccdcbf745
Khushi2427 <khushigupta16057@gmail.com>
2025-12-19 12:44:07 +0530

    Cors updated

--- server/app.js
+++ server/app.js
@@ -48,7 +48,10 @@ async function initializeServer() {
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [
+    "http://localhost:5173",
+    "https://face-matcher.vercel.app/"
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'OPTIONS']
 }));
